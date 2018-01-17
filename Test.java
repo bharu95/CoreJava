297409379b69e1a751b6d52109bf7c43341482e4
bharu95 <d.bhargav42@gmail.com>
2018-01-17 16:24:00 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.io.PrintStream;
+
+public class Test {
+
+	static PrintStream out=new PrintStream(System.out);
+	public static void main(String[] args) {
+		Test.out.println("hi");
+	}
+
+}
